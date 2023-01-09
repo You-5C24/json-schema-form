d7b56969e11e4865ca6f7c20a0e55156415790d9
you_5c24 <you_5c24@163.com>
2023-01-09 19:46:20 +0800

    feature: 初始化以及 新增 SchemaForm 和 SchemaItems

---
+++ lib/fields/NumberField.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: "NumberField",
+  setup(props) {
+    return () => <div>Number field</div>
+  },
+})
---
+++ lib/fields/StringField.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue"
+export default defineComponent({
+  name: "StringField",
+  setup(props) {
+    return () => <div>String field</div>
+  },
+})
